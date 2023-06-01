7c894666b5a4b76f03c7e580a15d77e29ca9edfd
Carl Allendorph <callendorph@gmail.com>
2023-06-01 13:43:32 -0700

    Added a new utility for generating a left aligned mask.

--- src/BitTools.vhd
+++ src/BitTools.vhd
@@ -14,6 +14,10 @@ function to_slv(val : integer; len : integer) return std_logic_vector;
 
 function to_std_logic(x: BOOLEAN) return std_logic;
 
+-- Make a left aligned width-bit mask. The
+--   returned std_logic_vector is `len` bits long.
+function make_left_mask(width : integer; len : integer) return std_logic_vector;
+
 end package;
 
 package body BitTools is
@@ -80,6 +84,23 @@ begin
     return '0';
   end if;
 end to_std_logic;
+
+function make_left_mask(
+  width : integer; len : integer
+  ) return std_logic_vector
+is
+  variable mask : std_logic_vector(len-1 downto 0);
+begin
+  mask := (others => '0');
+
+  for m in 0 to width-1 loop
+    mask := mask or to_slv( 2**m, len);
+  end loop;
+  mask := mask(width-1 downto 0) & to_slv(0, len-width);
+  return mask;
+end make_left_mask;
+
+
 
 
 end BitTools;
