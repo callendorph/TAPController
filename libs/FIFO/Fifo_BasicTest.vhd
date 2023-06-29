aa4a2e823e5b14255028df1153da2ead4e84af02
Carl Allendorph <callendorph@gmail.com>
2023-06-29 14:16:07 -0700

    [Refactor] Added new procedures to reduce duplication in tests.

--- libs/FIFO/Fifo_BasicTest.vhd
+++ libs/FIFO/Fifo_BasicTest.vhd
@@ -4,7 +4,7 @@ use work.FifoTbPkg.all;
 --
 -- Basic Fifo Unit test for the Fifo verification component.
 --   None of this is intended to be instantiated.
-architecture BasicTest of TestCtrl is
+architecture basic_test of TestCtrl is
 
   signal testActive  : boolean := TRUE;
   signal testDone    : integer_barrier := 1;
@@ -12,19 +12,7 @@ architecture BasicTest of TestCtrl is
 begin
   ControlProc : process
     begin
-      SetTestName("Fifo_BasicTest");
-      SetLogEnable(PASSED, TRUE);
-
-      wait for 0 ns;
-      -- These options are used to format the log output.
-      --   they don't affect the simulation in any way.
-      SetAlertLogOptions(WriteTimeLast => FALSE);
-      SetAlertLogOptions(TimeJustifyAmount => 16);
-      SetAlertLogJustify;
-
-      TranscriptOpen(OSVVM_RESULTS_DIR & "Fifo_BasicTest.txt");
-      -- Write to both the console and file.
-      SetTranscriptMirror(TRUE);
+      FifoTestSetup(basic_test'SIMPLE_NAME, OSVVM_RESULTS_DIR);
 
       -- Wait for Design Reset
       wait until RESET_n = '1';
@@ -43,7 +31,6 @@ begin
 
   TxProc : process
     variable fifoID : AlertLogIDType;
-    variable txCount, rxCount, obsCount : integer;
     variable widthVal, depthVal : integer;
     variable obs : Fifo_Out_DataType;
   begin
@@ -59,14 +46,7 @@ begin
 
     WaitForClock(FifoRec, 2);
 
-    GetCount(FifoRec, obsCount);
-    AffirmIfEqual(FifoID, obsCount, 0, "Fifo Count");
-
-    GetTxCount(FifoRec, txCount);
-    AffirmIfEqual(FifoID, txCount, 0, "Transmit Count");
-
-    GetRxCount(FifoRec, rxCount);
-    AffirmIfEqual(FifoID, rxCount, 0, "Read Count");
+    CheckFifoCounts(FifoRec, FifoID, 0, 0, 0);
 
     -- Write Values - These are full-handshake writes
     --  with de-assertion of ACK at the end.
@@ -74,79 +54,44 @@ begin
     FifoWriteHandshake(WR_EN, WR_ACK);
     wait until WR_ACK = '0';
 
-    GetCount(FifoRec, obsCount);
-    AffirmIfEqual(FifoID, obsCount, 1, "Fifo Count");
+    CheckFifoCounts(FifoRec, FifoID, 1, 1, 0);
 
     DIN <= X"F_00112233";
     FifoWriteHandshake(WR_EN, WR_ACK);
     wait until WR_ACK = '0';
 
-    GetTxCount(FifoRec, txCount);
-    AffirmIfEqual(FifoID, txCount, 2, "Transmit Count");
-
-    GetCount(FifoRec, obsCount);
-    AffirmIfEqual(FifoID, obsCount, 2, "Fifo Count");
-
-    GetRxCount(FifoRec, rxCount);
-    AffirmIfEqual(FifoID, rxCount, 0, "Read Count");
+    CheckFifoCounts(FifoRec, FifoID, 2, 2, 0);
 
     Get(FifoRec, obs);
     AffirmIfEqual(FifoID, obs, X"F_AABBCCDD", "Rx 1");
 
-    GetCount(FifoRec, obsCount);
-    AffirmIfEqual(FifoID, obsCount, 1, "Fifo Count");
-
-    GetRxCount(FifoRec, rxCount);
-    AffirmIfEqual(FifoID, rxCount, 1, "Read Count");
+    CheckFifoCounts(FifoRec, FifoID, 1, 2, 1);
 
     Get(FifoRec, obs);
     AffirmIfEqual(FifoID, obs, X"F_00112233", "Rx 2");
 
-    GetCount(FifoRec, obsCount);
-    AffirmIfEqual(FifoID, obsCount, 0, "Fifo Count");
-
-    GetRxCount(FifoRec, rxCount);
-    AffirmIfEqual(FifoID, rxCount, 2, "Read Count");
-
+    CheckFifoCounts(FifoRec, FifoID, 0, 2, 2);
 
     SendAsync(FifoRec, X"A_AABBCCDD");
     WaitForClock(FifoRec, 2);
     SendAsync(FifoRec, X"A_55443322");
     WaitForClock(FifoRec, 2);
 
-    GetCount(FifoRec, obsCount);
-    AffirmIfEqual(FifoID, obsCount, 2, "Fifo Count");
-
-    GetTxCount(FifoRec, txCount);
-    AffirmIfEqual(fifoID, txCount, 4, "Transmit Count");
+    CheckFifoCounts(FifoRec, FifoID, 2, 4, 2);
 
     FifoReadHandshake(DOUT, VALID, RD_EN, obs, tperiod_CLK);
     AffirmIfEqual(FifoID, obs, X"A_AABBCCDD", "Rx 3");
 
     WaitForClock(FifoRec, 2);
 
-    GetCount(FifoRec, obsCount);
-    AffirmIfEqual(FifoID, obsCount, 1, "Fifo Count");
-
-    GetRxCount(FifoRec, rxCount);
-    AffirmIfEqual(FifoID, rxCount, 3, "Read Count");
-
-    GetTxCount(FifoRec, txCount);
-    AffirmIfEqual(fifoID, txCount, 4, "Transmit Count");
+    CheckFifoCounts(FifoRec, FifoID, 1, 4, 3);
 
     FifoReadHandshake(DOUT, VALID, RD_EN, obs, tperiod_CLK);
     AffirmIfEqual(FifoID, obs, X"A_55443322", "Rx 4");
 
-    GetCount(FifoRec, obsCount);
-    AffirmIfEqual(FifoID, obsCount, 0, "Fifo Count");
+    CheckFifoCounts(FifoRec, FifoID, 0, 4, 4);
 
-    GetRxCount(FifoRec, rxCount);
-    AffirmIfEqual(FifoID, rxCount, 4, "Read Count");
-
-    GetTxCount(FifoRec, txCount);
-    AffirmIfEqual(fifoID, txCount, 4, "Transmit Count");
-
-    -- Test the option accesssors.
+    -- Test the option accessors.
     GetFifoWidth(FifoRec, widthVal);
     AffirmIfEqual(fifoID, widthVal, 36, "Width Check");
     GetFifoDepth(FifoRec, depthVal);
@@ -161,12 +106,12 @@ begin
 
   end process TxProc;
 
-end BasicTest;
+end basic_test;
 
 configuration Fifo_BasicTest of TbFifoVC is
   for TestHarness
     for TestDriver : TestCtrl
-      use entity work.TestCtrl(BasicTest);
+      use entity work.TestCtrl(basic_test);
     end for;
   end for;
 end Fifo_BasicTest;
